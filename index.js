ced625df0d6963bb3161487dc772814d5202b425
brajpatel <brajeshpatel0002@gmail.com>
2022-09-11 12:11:55 +0100

    Created players, add square event listeners

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //  toggle game theme
 const toggleTheme = (() => {
-    const body = document.querySelector('body')
-    const changeTheme = document.getElementById('change-theme-btn')
+    const body = document.querySelector('body');
+    const changeTheme = document.getElementById('change-theme-btn');
     changeTheme.addEventListener('click', () => {
-        body.classList.toggle('color')
+        body.classList.toggle('color');
     })
 })();
 
@@ -13,14 +13,57 @@ const createPlayer = (name, marker) => {
         name,
         marker
     }
-}
+};
 
 // gameboard
 const gameBoard = (() => {
+    const grid = document.getElementById('grid');
+    let board = [];
 
+    for(let i = 0; i < 9; i++) {
+        board.push(i);
+    }
+
+    board.forEach(() => {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        grid.appendChild(square)
+    })
 })();
 
 // game
 const game = (() => {
+    // display game messages
+    const gameMessage = document.getElementById('game-message')
+    const subText = document.getElementById('subtext')
 
+    const playerOne = createPlayer('Player One', 'player1');
+    const playerTwo = createPlayer('Player Two', 'player2');
+
+    let availableSpots = 9;
+    let num = Math.floor(Math.random() * 2);
+    let activePlayer = num === 0 ? playerOne : playerTwo;
+    let gameWon = false;
+
+    function checkWin() {
+
+    }
+
+    function alertPlayer() {
+
+    }
+
+    function nextPlayer() {
+
+    }
+
+    function declareTie() {
+
+    }
+
+    return {
+        alertPlayer,
+        nextPlayer,
+        declareTie
+    }
 })();
