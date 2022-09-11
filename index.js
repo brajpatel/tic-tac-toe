9afc6759b14276f83750e70e197f6993aed9fe95
brajpatel <brajeshpatel0002@gmail.com>
2022-09-11 12:14:32 +0100

    Add array of winning possibilities

--- index.js
+++ index.js
@@ -45,6 +45,17 @@ const game = (() => {
     let activePlayer = num === 0 ? playerOne : playerTwo;
     let gameWon = false;
 
+    const possibleWins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
     function checkWin() {
 
     }
